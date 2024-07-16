fb3a304c84986d35aeef82917134c5b8cca05c82
Peter Mwaura <kimmwaus@gmail.com>
2024-07-16 16:50:40 +0300

    update

---
+++ database/migrations/2024_07_15_071424_create_valid_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('valid_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->string('answer')->nullable();
+            $table->string('choice_1')->nullable();
+            $table->string('choice_2')->nullable();
+            $table->string('choice_3')->nullable();
+            $table->string('choice_4')->nullable();
+            $table->unsignedInteger('validated_by');
+            $table->boolean('passed')->default(false);
+            $table->string('category')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('valid_questions');
+    }
+};
